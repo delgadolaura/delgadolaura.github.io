1950557c47ac4f8cb2d148783e23345a089aa3d0
delgadolaura <55292582+delgadolaura@users.noreply.github.com>
2020-01-15 12:57:47 -0500

    Create myScripts.js

---
+++ js/myScripts.js
@@ -0,0 +1,11 @@
+var f;
+for (f = 0; f < 9; f++) {
+  var ans=Math.pow(2,f)
+  document.write(ans +"<br>");
+}
+
+var count=1 
+while(count<8){
+  var ans=Math.random(count); count++;
+  document.write(ans+"<br>")
+}

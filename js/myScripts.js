24a17cceb18168a751eb2a9256bc70ab2b0e053c
delgadolaura <55292582+delgadolaura@users.noreply.github.com>
2020-01-23 21:35:48 -0500

    Update myScripts.js

--- js/myScripts.js
+++ js/myScripts.js
@@ -13,8 +13,9 @@ while(count<8){
 function myLoop(){
 var start= document.getElementById("start").value;
 var end = document.getElementById("end").value;
-  
+  console.log(start);
 for(var i = start; i <= end; i++)
 {document.write('<p>' + i + '</p>');
 }
 }
+
